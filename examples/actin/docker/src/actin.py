8406d22f6f39c3934f6e4f2ee336ad38911d2323
Blair Lyons <blair208@gmail.com>
2021-04-12 17:38:10 -0700

    actin docker container runs locally

--- examples/actin/docker/src/actin.py
+++ examples/actin/docker/src/actin.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import pandas
 import argparse
 
@@ -16,6 +18,8 @@ def main():
         "params_path", help="the file path of an excel file with parameters")
     parser.add_argument(
         "data_column", help="the column index for the parameter set to use")
+    parser.add_argument(
+        "model_name", help="prefix for output file names", nargs='?', default="")
     args = parser.parse_args()
     parameters = pandas.read_excel(
         args.params_path, sheet_name="actin", usecols=[0, int(args.data_column)])
@@ -23,7 +27,7 @@ def main():
     parameters.transpose()
     run_name = list(parameters)[0]
     parameters = parameters[run_name]
-    parameters["name"] = run_name
+    parameters["name"] = args.model_name + "_" + run_name
     actin_simulation = ActinSimulation(parameters, True, True)
     actin_simulation.add_random_monomers()
     actin_simulation.add_random_linear_fibers()
@@ -31,6 +35,7 @@ def main():
         int(parameters["total_steps"]), parameters["timestep"])
     ActinVisualization.visualize_actin(
         "{}.h5".format(parameters["name"]), parameters["box_size"], [])
+    sys.exit(0)
 
 
 if __name__ == '__main__':
