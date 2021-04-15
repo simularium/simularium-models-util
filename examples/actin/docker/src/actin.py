20ba27661888fd361a40bf10482038c01129602c
Blair Lyons <blair208@gmail.com>
2021-04-15 11:53:49 -0700

    WIP adding memory reporting to actin

--- examples/actin/docker/src/actin.py
+++ examples/actin/docker/src/actin.py
@@ -5,10 +5,15 @@ import os
 import sys
 import pandas
 import argparse
+import psutil
+from time import sleep
 
 from simularium_models_util.actin import ActinSimulation
 from simularium_models_util.visualization import ActinVisualization
+from simularium_models_util import RepeatedTimer
 
+def report_memory_usage():
+    print(f"RAM percent used: {psutil.virtual_memory()[2]}")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -19,11 +24,11 @@ def main():
     parser.add_argument(
         "data_column", help="the column index for the parameter set to use")
     parser.add_argument(
-        "model_name", help="prefix for output file names", nargs='?', default="")
+        "model_name", help="prefix for output file names", nargs="?", default="")
     args = parser.parse_args()
     parameters = pandas.read_excel(
         args.params_path, sheet_name="actin", usecols=[0, int(args.data_column)])
-    parameters.set_index('name', inplace=True)
+    parameters.set_index("name", inplace=True)
     parameters.transpose()
     run_name = list(parameters)[0]
     parameters = parameters[run_name]
@@ -33,10 +38,14 @@ def main():
     actin_simulation = ActinSimulation(parameters, True, True)
     actin_simulation.add_random_monomers()
     actin_simulation.add_random_linear_fibers()
-    actin_simulation.simulation.run(
-        int(parameters["total_steps"]), parameters["timestep"])
-    ActinVisualization.visualize_actin(
-        "{}.h5".format(parameters["name"]), parameters["box_size"], [])
+    rt = RepeatedTimer(1, report_memory_usage)
+    try:
+        actin_simulation.simulation.run(
+            int(parameters["total_steps"]), parameters["timestep"])
+        ActinVisualization.visualize_actin(
+            "{}.h5".format(parameters["name"]), parameters["box_size"], [])
+    finally:
+        rt.stop()
     sys.exit(0)
 
 
